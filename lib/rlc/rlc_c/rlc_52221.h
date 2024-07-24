8b153543ea4c30d5b19a3e16ae498a11b84b43d9
Marek Naumiak <02nmarek@gmail.com>
2024-07-24 16:51:47 +0200

    komentarz

--- lib/rlc/rlc_c/rlc_52221.h
+++ lib/rlc/rlc_c/rlc_52221.h
@@ -3,7 +3,7 @@
 #ifndef RLC_52221
 #define RLC_52221
 
-//
+//potrzebne structy 
 struct rlc_rx_um_state
 {
     int rx_next_highest;
@@ -14,7 +14,7 @@ struct rlc_rx_um_entity
     struct rlc_rx_um_state st;
     const uint32_t um_window_size;
 };
-
+//deklaracja funkcji
 int sn_in_reassembly_window_c(const int sn, struct rlc_rx_um_entity* entity);
 #endif
 
